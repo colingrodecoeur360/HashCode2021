22d350ca12c88b3cf60500879fae182da099ea02
Colin Grodecoeur <colin.grodecoeur@360learning.com>
2021-02-25 22:34:27 +0100

    remove stuff

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import { loadFile } from "./loader";
 import { saveFile } from "./saver";
 import { buildOutput } from "./solution";
-import { computeScore } from "./score";
 
-// run("a");
-// run("b");
-// run("c");
+run("a");
+run("b");
+run("c");
 run("d");
 run("e");
 run("f");
@@ -14,8 +13,6 @@ run("f");
 function run(file: string) {
     const problem = loadFile(`inputs/${file}.txt`);
 
-    // console.log(problem.paths)
     const solution = buildOutput(problem);
-    // console.log(file, computeScore(problem, solution));
     saveFile(`outputs/${file}.txt`, solution);
 }
