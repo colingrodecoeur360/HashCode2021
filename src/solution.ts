69c25727ab3dffbf3604539a8ec13f1a9c43f616
Colin Grodecoeur <colin.grodecoeur@360learning.com>
2021-02-25 22:33:41 +0100

    last solution

--- src/solution.ts
+++ src/solution.ts
@@ -6,30 +6,16 @@ export function buildOutput(problem: Problem): Solution {
 
     const streetsByName: Record<string, Street> = {};
 
-    const times: number[] = [];
-
     problem.streets.forEach(street => {
-        times.push(street.travelTime);
         streetsByName[street.name] = street;
     });
 
-    // console.log(times.sort((a, b) => a - b));
-    // console.log(times.sort((a, b) => a - b).reverse());
-
-
     const streetFrequencies: Record<string, number> = {};
-    let maxPossible = 0
-
-    const scores: number[] = [];
-    problem.paths.forEach(path => {
-        const duration = _.sum(path.streetNames.slice(1).map(streetName => streetsByName[streetName].travelTime));
-        const maxScore = problem.BonusPoints + problem.Duration - duration;
-        scores.push(maxScore);
-    });
-
-    const sortedScores = scores.sort((a, b) => a - b);
 
     problem.paths.forEach((path) => {
+        const duration = _.sum(path.streetNames.map(streetName => streetsByName[streetName].travelTime));
+        if (duration > 0.6 * problem.Duration) { return; }
+
         path.streetNames.forEach(streetName => {
             if (! streetFrequencies[streetName]) {
                 streetFrequencies[streetName] = 0;
@@ -49,15 +35,14 @@ export function buildOutput(problem: Problem): Solution {
     return {
         nbIntersections: Object.keys(intersections).length,
         schedules: Object.entries(intersections).map(([intersectionId, streets]) => {
-            const orderedStreets = streets.sort((s1, s2) => streetFrequencies[s1.name] - streetFrequencies[s2.name]);
+            const orderedStreets = streets.sort((s1, s2) => streetFrequencies[s1.name] / s1.travelTime - streetFrequencies[s2.name] / s2.travelTime);
             return {
                 intersection: parseInt(intersectionId, 10),
                 nbStreets: streets.length,
                 pattern: orderedStreets.map((street, streetIndex) => {
                     return {
                         streetName: street.name,
-                        duration: 25
-                        // duration: Math.min(streetIndex + 1, streetFrequencies[street.name])
+                        duration: Math.min(streetIndex + 1, Math.ceil(streetFrequencies[street.name] / 100))
                     }
                 })
             };
